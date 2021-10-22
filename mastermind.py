23b1323fea3868cadeda7da04d7efd9cab5fdc90
Riley Harbick <rileyharbick@gmail.com>
2021-10-22 04:14:48 -0400

    Initial singleplayer command-line version

--- mastermind.py
+++ mastermind.py
@@ -1,4 +1,5 @@
 import random
+import copy
 
 def generate_code():
     colors = ['red', 'green', 'yellow', 'blue', 'white', 'black']
@@ -11,23 +12,33 @@ def generate_code():
 def check_guess(guess, code):
     response = []
     index = 0
+    code_copy = copy.copy(code)
     for color in guess:
-        if color in code and index == code.index(color):
+        if color in code_copy and index == code_copy.index(color):
             response.append('B')
-        elif color in code:
+            code_copy[index] = 'X'
+        elif color in code_copy:
             response.append('W')
         index += 1
 
     if response == ['B', 'B', 'B', 'B']:
-        return 'you win!'
-    return response
+        return (True, response)
+    else:
+        return (False, response)
 
-
-    
-
-
+win = False
 code = generate_code()
-print(check_guess(['yellow', 'black', 'black', 'white'], ['yellow', 'black', 'black', 'white']))
+#print(f'DEBUG: CODE = {code}')
+
+print("Welcome to Mastermind!")
+while (not win):
+    print("Make a guess:")
+    guess = input().split()
+    guess = [x.lower() for x in guess]
+    result = check_guess(guess, code)
+    print(result[1])
+    win = result[0]
+print("You win!")
 
 
 
